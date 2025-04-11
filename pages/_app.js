b6d2c1afbd419bf29636bb3c1e0775f08ab8d8a4
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-11 14:56:03 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Head from 'next/head';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { ThemeContextProvider } from '../contexts/ThemeContext';
 import { SermonContextProvider } from '../contexts/SermonContext';
 import createEmotionCache from '../utils/createEmotionCache';
-import '../styles/globals.css';  // Asegúrate de que esta línea esté presente
+import '../styles/globals.css';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -13,6 +14,11 @@ export default function MyApp(props) {
 
   return (
     <CacheProvider value={emotionCache}>
+      <Head>
+        <title>PredicaHoy - Generador de Sermones</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="description" content="Generador de sermones cristianos personalizados" />
+      </Head>
       <ThemeContextProvider>
         {(theme) => (
           <ThemeProvider theme={theme}>

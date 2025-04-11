7bf6e4cab52eb246998a997eff3d6c4983b0eb29
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-11 01:12:28 -0600

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

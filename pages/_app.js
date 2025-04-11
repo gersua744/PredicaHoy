273a85c437c1411a68414153b50218173b4a592c
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-11 13:18:37 -0600

    Delete pages directory

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';  // Asegúrate de que esta línea esté presente
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
-

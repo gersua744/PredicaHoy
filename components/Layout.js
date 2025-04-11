8e09358b02439012e7093fcdaf5e093f30f53f6c
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-11 14:54:28 -0600

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
 import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <Box sx={{ 
-      display: 'flex', 
-      flexDirection: 'column',
-      minHeight: '100vh',
-      bgcolor: (theme) => theme.palette.background.default
-    }}>
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        flexDirection: 'column',
+        minHeight: '100vh'
+      }}
+    >
       <Header />
-      <Box component="main" sx={{ flexGrow: 1 }}>
+      <Box component="main" sx={{ flexGrow: 1, py: 4 }}>
         {children}
       </Box>
       <Footer />

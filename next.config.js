44b40a7fd8e8c27ee5f78c55d65c5a140d88cacf
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-12 00:54:09 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
   i18n: {
     locales: ['es'],
     defaultLocale: 'es',
   },
-  // Configuración para Netlify
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
-  },
-  // Habilitar caché de compilación
-  experimental: {
-    turbotrace: {
-      logLevel: 'error',
-    },
+  images: {
+    domains: ['images.unsplash.com'],
   },
 }
 

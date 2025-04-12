78ff168b567ddc7c7134736ad3438f637a2c82b6
Gerson Pérez <gersua.garcia@gmail.com>
2025-04-11 23:08:21 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
   i18n: {
     locales: ['es'],
     defaultLocale: 'es',
-  },
+  }
 }
 
 module.exports = nextConfig
